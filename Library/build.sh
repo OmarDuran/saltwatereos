6e369b592a1694076da8bc4cdd6e1644f06218a9
Omar Duran <omaryesiduran@gmail.com>
2026-03-17 02:21:30 -0700

    wip: tbb parallelization

--- Library/build.sh
+++ Library/build.sh
@@ -5,16 +5,17 @@ set -x
 rootpath=${PWD}
 export GSL_ROOT_DIR=${rootpath}/gsl-2.6/gsl
 # 1. build gsl
-build_gsl=$rootpath/gsl-2.6/src/build 
+build_gsl=$rootpath/gsl-2.6/src/build
 mkdir $build_gsl
-cd $build_gsl 
+cd $build_gsl
 ../configure --prefix=${GSL_ROOT_DIR}
 make -j 8 #using 8 threads
 make install
 
 # 2. build swEOS
-build_swEOS=$rootpath/build 
+build_swEOS=$rootpath/build
 mkdir $build_swEOS
 cd $build_swEOS
-cmake ..
+HOST_ARCH=$(uname -m)
+cmake -DCMAKE_OSX_ARCHITECTURES=${HOST_ARCH} ..
 make install

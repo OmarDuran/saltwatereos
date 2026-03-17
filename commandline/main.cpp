3e369feaf2288f5c2f2a56f28b99c2133d35a653
Omar Duran <omaryesiduran@gmail.com>
2026-03-17 01:41:52 -0700

    Update main.cpp

--- commandline/main.cpp
+++ commandline/main.cpp
@@ -1,6 +1,13 @@
 
 #include "SWEOSbash.h"
+#include <iostream>
 int main( int argc, char** argv )
 {
+  std::cout << "\033[36m"
+            << "  Fork & TBB parallelization:\n"
+            << "    Omar Duran, University of Bergen (UiB), 2026\n"
+            << "  Original source:\n"
+            << "    github.com/zguoch/salern\n"
+            << "\033[0m" << std::endl;
   SWEOSbash::bash_run(argc, argv);
 }
